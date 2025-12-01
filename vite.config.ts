e870e879feac61dc11129e2fc0e1e518647e88e0
MICHAEL ABBEY <Mikteyeabbey@yahoo.com>
2025-12-01 09:33:37 +0000

    done

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const isProduction = mode === 'production';
     return {
+      base: isProduction ? '/icons-of-influence/' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
